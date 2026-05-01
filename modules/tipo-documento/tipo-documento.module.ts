50f79dae271da72f5b2b7ae41a67807812a78320
CristhianImpa2 <criscaimp@gmail.com>
2026-05-01 00:05:31 -0500

    Primer commit: proyecto nest corregido

---
+++ modules/tipo-documento/tipo-documento.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TipoDocumentoController } from './tipo-documento.controller';
+import { TipoDocumentoService } from './tipo-documento.service';
+
+@Module({
+  controllers: [TipoDocumentoController],
+  providers: [TipoDocumentoService]
+})
+export class TipoDocumentoModule {}
