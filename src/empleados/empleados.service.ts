ca20fa8d474626745d7bdd34c486d70132fc3202
Kilesasa <kilesgomez33@gmail.com>
2026-05-05 16:21:51 -0500

    serivicios actualizados

--- src/empleados/empleados.service.ts
+++ src/empleados/empleados.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+interface Empleado { // Definimos los types para utilizar en la clase. 
+    id: number,
+    nombre: string
+
+}
 
 @Injectable()
-export class EmpleadosService {}
+export class EmpleadosService {
+    private empleados: Empleado[] = [ //Estamos creando un arry que es te tipo Empleados
+        {id:1, nombre: 'Jose Carlos'},
+        {id:2, nombre: 'Marco Jose'}, 
+    ];
+    
+    findAll(): Empleado[] { // indicamos que esperamos un arrglo como rector de tipo Empleado
+        return this.empleados; 
+    }
+
+    findOne(id: number): Empleado {
+
+        const empleado = this.empleados.find(parametro => parametro.id === id );
+        if (!empleado){
+            throw new NotFoundException(`Empleado con id ${id} no encontrado`);
+        } 
+        return empleado;
+
+    }
+
+    
+
+
+}
