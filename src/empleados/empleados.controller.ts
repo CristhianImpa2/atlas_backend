1a080d6fcc55afea8eca1422c3a0a07c25f941cc
Kilesasa <kilesgomez33@gmail.com>
2026-05-13 23:30:24 -0500

    CRUD de empleados

--- src/empleados/empleados.controller.ts
+++ src/empleados/empleados.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { EmpleadosService, Empleado}  from './empleados.service'; 
+import { retry } from 'rxjs';
 @Controller('empleados')
-export class EmpleadosController {}
+export class EmpleadosController {
+    constructor(private readonly empleadosService:EmpleadosService){} // vamos a utilizar la instancia de el contendor de dependencias. indicamos que vamos a utilizar la instancia del inyectable. 
+
+    @Get()
+    getAll(): Empleado[]{
+
+        return this.empleadosService.findAll(); 
+
+    }
+
+    @Get(':id') // Se ejecuta el metodo, cuando resivamos una url, que concuerde con la url y elemento mas. 
+    getOne(@Param('id',ParseIntPipe) id: number){ // @Param, me ayuda a capturar el id que venia con el id de url. que campuro el get. 
+        return this.empleadosService.findOne(id);
+    }
+
+}
