b3903b2b2933584037ce5982d89f35d0cae56bce
Kilesasa <kilesgomez33@gmail.com>
2026-05-21 21:01:49 -0500

    CRUD, empleados

--- src/empleados/empleados.controller.ts
+++ src/empleados/empleados.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { EmpleadosService, Empleado}  from './empleados.service'; 
 import { retry } from 'rxjs';
 @Controller('empleados')
@@ -16,5 +16,33 @@ export class EmpleadosController {
     getOne(@Param('id',ParseIntPipe) id: number){ // @Param, me ayuda a capturar el id que venia con el id de url. que campuro el get. 
         return this.empleadosService.findOne(id);
     }
+    
+    @Post() 
+    Create(@Body() body:CreateEmpleadoDto): Empleado {
+        retrun this.empleadosService.create(body); 
+    }
 
+    
+    @Put(':id')// Cuando la ruta indique put, mas un parametro dinamico. 
+    Update(// Ejeuctamos aqui. 
+        @Parm('id', ParseIntPipe) id: number, // extrameos el id, de la ruta dinamica, lo hacemos un numero y decimos que es de tipo number. 
+        @Body( ) body:Upadate, // Tomamos el body que es donde viene la actualizacion. 
+    ): Empleado{ // Luego de resivir toda la ifo, y tiparla, hacemos la ejecucion el service pasando los respectivos parametros necesarios para el metodo update en el service. 
+
+        return this.empleadosService.update(id, body);
+
+    }
+
+    
+    
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number): void {
+        return this.empleadosService.remove(id);
+    }
+}
+
+    
+    
+    
+        
 }
